58cb47fa83011c67ddcd68a420bc4d2d784ec9c7
huone1 <huwanxing@huawei.com>
2021-03-25 15:18:15 +0800

    fix compile

--- pkg/args/args.go
+++ pkg/args/args.go
@@ -32,7 +32,7 @@ func NewArgument() *Argument {
 func (args *Argument) AddFlags(fs *pflag.FlagSet) {
 	fs.DurationVar(&args.CheckInterval, "check-period", defaultCheckInterval, "Burst to use while talking with kubernetes apiserver")
 	fs.StringVar(&args.KubeletConf, "kubelet-conf", args.KubeletConf, "Path to kubelet configure file")
-	fs.StringVar(&args.NumaPath, "device-path", args.NumaPath, "Path to numa node information")
+	fs.StringVar(&args.DevicePath, "device-path", args.DevicePath, "Path to device information")
 	fs.StringVar(&args.CpuMngstate, "cpu-manager-state", args.CpuMngstate, "Path to cpu_manager_state")
 
 	fs.StringVar(&args.KubeClientOptions.Master, "master", args.KubeClientOptions.Master, "The address of the Kubernetes API server (overrides any value in kubeconfig)")
